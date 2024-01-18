04934ea5f9058817d51c516041746591c2e72690
avawilliams <avawilliams@AVAWILLIAMS6D70>
2024-01-17 18:15:09 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+namespace Mission2
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            //GETTING THE USER INPUT FOR NUMBER OF ROLLS
+            System.Console.WriteLine("Welcome to the dice throwing simulator!");
+            System.Console.WriteLine("How many dice rolls would you like to simulate?");
+
+            //CONVERTING USER INPUT FROM STRING TO INTEGER
+            int numRolls = Convert.ToInt32(System.Console.ReadLine());
+
+            //CREATING A THROWING DICE OBJECT AND PLACING IT IN AN ARRAY OF RESULTS
+            ThrowingDice td = new ThrowingDice();
+            int[] results = td.SimulateThrowingDice(numRolls);
+
+            //MAKING THE HISTOGRAM
+            Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+            Console.WriteLine($"Total number of rolls = {numRolls}.");
+
+            //ITERATING THROUGH THE RESULTS ARRAY AND CREATING PERCENTAGES
+            for (int i = 0; i < 11; i++)
+            {
+                int percentage = results[i] * 100 / numRolls;
+                Console.WriteLine($"{i + 2}: {new string('*', percentage)}");
+            }
+
+            //GOODBYE PHRASE
+            Console.WriteLine("Thanks for playing!");
+        }
+
+    }
+}
+
+
---
+++ ThrowingDice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    internal class ThrowingDice
+    {
+        //CREATE A RANDOM OBJECT
+        private static Random random = new Random();
+
+        //CREATE METHOD FOR SIMULATING DICE THROWS
+        public int[] SimulateThrowingDice(int numRolls)
+        {
+            //CREATING AN EMPTY ARRAY(TO HOUSE DICE THROWS RESULTS/11 SLOTS FOR 2-12)
+            int[] results = new int[11];
+
+            //ROLL DICE FOR HOW MANY TIMES THE USER WANTED TO
+            for (int i = 0; i < numRolls; i++)
+            {
+                //ROLL THE DIE (1-6 AS OPTIONS)
+                int dice1 = random.Next(1, 7);
+                int dice2 = random.Next(1, 7);
+
+                //ADD UP BOTH DICE
+                int sum = dice1 + dice2;
+
+                //ADD ONE TO THE NUMBER THAT WAS ROLLED IN THE ARRAY
+                results[sum - 2]++;
+            }
+
+            //RETURN THE ARRAY OF RESULTS
+            return results;
+        }
+
+    }
+}
